4ffb53948d2b34c2566d71301eee2a58a5c579e5
Ekramul Islam Sumon <38817030+sumoninfo@users.noreply.github.com>
2023-07-03 15:29:31 +0600

    networks and phpmyadmin add

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,4 @@ FROM php:7.4-apache
 RUN docker-php-ext-install mysqli
 COPY . /var/www/html
 EXPOSE 80
+RUN echo "opcache.enable=0" >> /usr/local/etc/php/conf.d/docker-php-ext-opcache.ini
--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 // Connect to the database and perform queries
-$host = 'localhost';
-$user = 'admin';
-$password = '1234';
-$database = 'test-docker-db';
+
+echo "hello php ";
+
+$host = "db";
+$user = "root";
+$password = "1234";
+$database = "test-docker-db";
 
 $connection = new mysqli($host, $user, $password, $database);
 
@@ -11,12 +14,12 @@ if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
 }
 
-$sql = "SELECT * FROM mytable";
+$sql = "SELECT * FROM users";
 $result = $connection->query($sql);
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        echo $row['column_name'] . "<br>";
+        echo $row['name'] . "<br>";
     }
 } else {
     echo "No rows found";
